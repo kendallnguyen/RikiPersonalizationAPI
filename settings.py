93938928309af299add328ff54aa784c99cb18be
kendall <nguyenk6@mymail.nku.edu>
2020-04-17 19:31:18 -0400

    backend done, front end not done

--- settings.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///D:\\School\\NKU\\CSC540\\RikiPersonalizationAPI\\database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
